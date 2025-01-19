5b79a0ad18bbf1b4ad3c6fe30e68c25c03ecd444
Totsnuk <evanmaywald2@gmail.com>
2025-01-18 18:23:47 -0600

    Update IsEnemyInArea.gd

--- ai/tasks/IsEnemyInArea.gd
+++ ai/tasks/IsEnemyInArea.gd
@@ -4,7 +4,8 @@ extends BTCondition
 
 func _tick(delta: float) -> Status:
 	if "vision_component" in agent:
-		var target = agent.vision_component
-		if target:
+		var vision : VisionComponent = agent.vision_component
+		var has_target = vision.visible_targets.size() > 0
+		if has_target:
 			return SUCCESS
 	return FAILURE
