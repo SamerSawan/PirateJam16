a58718b31a66f7f76853a072591b6c7c8057d745
nmasudi17 <nmasudi12341234@gmail.com>
2025-01-20 18:12:18 -0500

    change back everything and add different sprites

--- corpse.gd
+++ corpse.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -10,6 +11,8 @@ func _process(delta):
 		$EButton.visible = true;
 	if entered and Input.is_action_just_pressed("interact"):
 		infest();
+	if not entered:
+		$EButton.visible = false;
 
 var entered = false;
 func _on_area_2d_body_entered(body):
@@ -18,5 +21,6 @@ func _on_area_2d_body_entered(body):
 
 func _on_area_2d_body_exited(body):
 	entered = false; # Replace with function body.
+
 func infest():
 	print("infested")
