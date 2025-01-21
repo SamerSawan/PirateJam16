3419f4a2d13a0220301c87723d41d80a07e8f8b3
Totsnuk <evanmaywald2@gmail.com>
2025-01-21 08:59:25 -0600

    Create PrimaryAttack.gd

---
+++ ai/tasks/PrimaryAttack.gd
@@ -0,0 +1,14 @@
+extends BTAttack
+
+# Primary attackkkkk
+
+var _agent : Creature
+func _enter() -> void:
+	if agent is Creature:
+		_agent = agent
+
+func _tick(delta: float) -> Status:
+	if agent:
+		_agent.primary_attack.trigger.emit(direction)
+		return SUCCESS
+	return FAILURE
