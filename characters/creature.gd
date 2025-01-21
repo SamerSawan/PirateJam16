3b2737acba885d798126e00d43546057d3ff6ec2
Totsnuk <evanmaywald2@gmail.com>
2025-01-21 08:59:06 -0600

    Update creature.gd
    added current_enemy_direction and ready for primary and secondary attacks

--- characters/creature.gd
+++ characters/creature.gd
@@ -7,5 +7,18 @@ class_name Creature
 @export var navigation_agent : NavigationAgent2D
 @export var navigation_component : NavigationComponent
 
-@export var is_able_to_attack : bool = true
+@export var primary_attack : Attack
+@export var secondary_attack : Attack
+
 @export var is_friendly_fire : bool = false
+
+@export var current_enemy_direction : Vector2
+
+func _ready():
+	# duplicate code bc may change in the future, we shall see
+	primary_attack.attack_range.enemy_in_range.connect(func(enemy):
+		current_enemy_direction = global_position.direction_to(enemy.global_position)
+	)
+	secondary_attack.attack_range.enemy_in_range.connect(func(enemy):
+		current_enemy_direction = global_position.direction_to(enemy.global_position)
+	)
