7568e9ff710a5415cb3ec820d3c2eedea861ceac
Totsnuk <evanmaywald2@gmail.com>
2025-01-21 07:54:31 -0600

    made attack range use the attacks user instead

--- scripts/Attack/attack_range.gd
+++ scripts/Attack/attack_range.gd
@@ -4,8 +4,6 @@ class_name AttackRange
 signal enemy_in_range(target : Node)
 signal friendly_in_range(target : Node)
 
-@export var user : Creature
-
 @export var attack : Attack
 @export var attack_area : Area2D ## Area that we detect if enemies are within range of attack, if they are, send target_in_range signal
 
@@ -14,7 +12,7 @@ func _ready() -> void:
 
 func _on_body_entered(node : Node):
 	if node is Creature:
-		var user_team = GameState.get_first_team_of_node(user)
+		var user_team = GameState.get_first_team_of_node(attack.user)
 		var friendlies = GameState.get_friendly_to_team(user_team)
 		var hostiles = GameState.get_hostile_to_team(user_team)
 		var node_team = GameState.get_first_team_of_node(node)
