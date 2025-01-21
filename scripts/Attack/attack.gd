aa04b48a0e4e6d71a1b0230e21c099b59b7d5612
Totsnuk <evanmaywald2@gmail.com>
2025-01-21 07:54:49 -0600

    unspecified trigger, added comment

--- scripts/Attack/attack.gd
+++ scripts/Attack/attack.gd
@@ -1,7 +1,7 @@
 extends Node2D
 class_name Attack
 
-signal trigger(direction : Vector2)
+signal trigger ## use trigger to send whatever data you need for the attack, direction, etc.
 
 @export var user : Creature
-@export var attack_range : AttackRange
+@export var attack_range : AttackRange # Holds an area that defines where attack can be triggered
