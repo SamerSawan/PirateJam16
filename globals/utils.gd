d92b860a0d4fb9ca5488595cb2ab28d8df0c06ba
Totsnuk <evanmaywald2@gmail.com>
2025-01-21 05:31:48 -0600

    Update utils.gd
    remove old unneeded

--- globals/utils.gd
+++ globals/utils.gd
@@ -1,17 +1,5 @@
 extends Node2D
 
-#region Collision Layers
-enum COLLISIONLAYERS{
-	GROUND,
-	Layer2,
-	Layer3,
-	Layer4,
-	PLAYER,
-	ENEMY,
-	FRIENDLY,
-	Layer8
-}
-#endregion
 #region Navigation
 @onready var DEFAULT_RID = get_world_2d().get_navigation_map()
 #endregion
