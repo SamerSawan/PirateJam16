5b15ce9e4744679cf108c733ae4eb87b5f74c8f1
Totsnuk <evanmaywald2@gmail.com>
2025-01-25 01:09:25 -0600

    Update attack.gd
    add example for trigger

--- scripts/Attack/attack.gd
+++ scripts/Attack/attack.gd
@@ -5,3 +5,9 @@ signal trigger ## use trigger to send whatever data you need for the attack, dir
 
 @export var user : Creature
 @export var attack_range : AttackRange # Holds an area that defines where attack can be triggered
+
+#func _ready() -> void:
+	#trigger.connect(_on_trigger)
+
+#func _on_trigger():
+	#pass
