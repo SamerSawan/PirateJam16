c971928a6fce488e72e9e6e5d0a43d756e4ee4e3
Totsnuk <evanmaywald2@gmail.com>
2025-01-25 01:05:45 -0600

    Update utils.gd
    added get_closest_body util function

--- globals/utils.gd
+++ globals/utils.gd
@@ -26,3 +26,18 @@ enum SPEED_MOD_KEY {
 	#var speed_value = SPEED_MODS[selected_speed_mod]
 	#print("Selected Speed Modifier: ", selected_speed_mod, " = ", speed_value)
 #endregion
+
+## returns the closest body to a node in global_position
+func get_closest_body(node : Node2D, bodies : Array[Node2D]):
+	var closest_body = bodies.reduce(
+		func(body1 : CharacterBody2D, body2 : CharacterBody2D):
+		var distance1 = node.global_position.distance_to(body1.global_position)
+		var distance2 = node.global_position.distance_to(body2.global_position)
+		
+		# Return the body with the smallest distance
+		if distance1 < distance2:
+			return body1
+		else:
+			return body2
+	)
+	return closest_body
