8d41487cd563232dbbbcfa15be2049a399d31c0a
Totsnuk <evanmaywald2@gmail.com>
2025-01-25 01:12:03 -0600

    fix for humans

--- ai/tasks/AbleToPrimaryAttack.gd
+++ ai/tasks/AbleToPrimaryAttack.gd
@@ -5,9 +5,9 @@ extends BTCondition
 func _tick(delta: float) -> Status:
 	if agent is Creature:
 		var _agent : Creature = agent
-		if _agent.current_enemy_direction is Vector2:
-			#print("\n")
-			#print("able to primary attack")
-			#print("\n")
+		var enemies_in_range = _agent.primary_attack.attack_range.get_enemies_in_range()
+		if enemies_in_range.size() > 0:
+			print("Enemies in range of Primary Attack: " + str(enemies_in_range))
+			print("Able to primary attack")
 			return SUCCESS
 	return FAILURE
