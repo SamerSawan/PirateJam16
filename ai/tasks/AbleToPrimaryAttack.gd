2430d153a85778b5ea24afdf524a307021fd41b3
Totsnuk <evanmaywald2@gmail.com>
2025-01-25 01:26:26 -0600

    Update AbleToPrimaryAttack.gd
    moved print

--- ai/tasks/AbleToPrimaryAttack.gd
+++ ai/tasks/AbleToPrimaryAttack.gd
@@ -6,8 +6,8 @@ func _tick(delta: float) -> Status:
 	if agent is Creature:
 		var _agent : Creature = agent
 		var enemies_in_range = _agent.primary_attack.attack_range.get_enemies_in_range()
+		print("Enemies in range of Primary Attack: " + str(enemies_in_range))
 		if enemies_in_range.size() > 0:
-			print("Enemies in range of Primary Attack: " + str(enemies_in_range))
 			print("Able to primary attack")
 			return SUCCESS
 	return FAILURE
