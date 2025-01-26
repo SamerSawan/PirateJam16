e7a49261189166744deb61a43a3b41c69a117164
Totsnuk <evanmaywald2@gmail.com>
2025-01-25 23:25:28 -0600

    Update attack_range.gd

--- scripts/Attack/attack_range.gd
+++ scripts/Attack/attack_range.gd
@@ -21,12 +21,13 @@ class_name AttackRange
 
 func get_enemies_in_range() -> Array[Node2D]:
 	var bodies : Array[Node2D] = attack_area.get_overlapping_bodies()
-	print("Bodies in attack area: " + str(bodies))
-	return bodies.filter(func(body): 
-		return body is Creature and GameState.is_user_hostile_to_node(attack.user, body)
+	var enemy_creatures : Array[Node2D] = bodies.filter(func(body): 
+		return GameState.is_user_hostile_to_node(attack.user, body)
 	)
+	print("Enemies in range of attack area: " + str(enemy_creatures))
+	return enemy_creatures
 
 func get_closest_enemy(node) -> Creature:
 	if node is Creature:
-		return Utils.get_closest_body(attack.user, node.get_overlapping_bodies())
+		return Utils.get_closest_body(attack.user, attack_area.get_overlapping_bodies())
 	return
