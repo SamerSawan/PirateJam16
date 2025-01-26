f0c5dbb306d39fbea7d3dc834a546cb555ae4bf4
Totsnuk <evanmaywald2@gmail.com>
2025-01-25 23:28:42 -0600

    militia spear attack now uses @export for animation player instead of in the behavior tree

--- characters/human_militia/militia_spear_attack.gd
+++ characters/human_militia/militia_spear_attack.gd
@@ -4,9 +4,18 @@ class_name MilitiaSpearAttack
 signal change_orientation
 
 @export var orientation_handler : OrientationHandler
+@export var animation_player : AnimationPlayer
 
 func _ready() -> void:
 	trigger.connect(_on_trigger)
 
-func _on_trigger(enemy : Creature):
-	change_orientation.emit(self.global_position.direction_to(enemy.global_position))
+func _on_trigger(enemy : Creature, attack_type : StringName):
+	match attack_type:
+		&"PrimaryAttack":
+			var pos = self.get_position()
+			var enemy_direction = pos.direction_to(self.to_local(enemy.global_position))
+			#print("Enemy Direction local:" + str(self.to_local(enemy_direction)))
+			change_orientation.emit(enemy_direction)
+			animation_player.play(attack_type)
+			await animation_player.current_animation_changed
+			trigger_finished.emit()
