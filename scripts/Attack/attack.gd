c13bdf43c30b63fb93e70016ba3db84d61a40dc3
Totsnuk <evanmaywald2@gmail.com>
2025-01-25 23:25:43 -0600

    Update attack.gd
    trigger_finished signal

--- scripts/Attack/attack.gd
+++ scripts/Attack/attack.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name Attack
 
 signal trigger ## use trigger to send whatever data you need for the attack, direction, etc.
+signal trigger_finished ## optional signal for when finishing trigger, mainly for use in limboai with the await animation finished
 
 @export var user : Creature
 @export var attack_range : AttackRange # Holds an area that defines where attack can be triggered
