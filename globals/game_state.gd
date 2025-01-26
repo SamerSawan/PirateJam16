e4c09ff369ea4d19e8fb59881787175bd2c31040
Totsnuk <evanmaywald2@gmail.com>
2025-01-25 23:26:24 -0600

    Update game_state.gd
    fix for all team methods

--- globals/game_state.gd
+++ globals/game_state.gd
@@ -17,72 +17,77 @@ const Teams : Dictionary = {
 	}
 }
 
-## Removes all existing teams from node
-func reset_teams_of_node(node : Node, #teams_to_exclude : Array[StringName] = []
-):
-	node.get_groups().all(
-		func(group_name):
-			if group_name in Teams:
-				node.remove_from_group(group_name)
-	)
-
 ## Sets nodes team safely
-func set_team_of_node(node : Node, team_name : StringName):
+func set_team_of_node(node : Node2D, team_name : StringName) -> void:
 	if team_name in Teams:
 		node.add_to_group(team_name)
 
-func get_first_team_of_node(node : Node) -> Dictionary:
-	var team = {}
-	node.get_groups().any(
-		func(group_name : StringName):
-			if group_name in Teams: # if node has a team
-				team[group_name] = Teams[group_name]
-	)
-	#print("\n")
-	#print("First team of %s: " % node.name)
-	#print("Team: %s" % str(team))
-	#print("Node: %s" % str(node))
-	#print("\n")
-	
-	return team
+## Removes first occurance of team_name
+func remove_team_of_node(node : Node2D, team_name : StringName) -> void:
+	if team_name in Teams:
+		node.remove_from_group(team_name)
 
-## Return dict of teams that a node has
-func get_teams_of_node(node : Node) -> Dictionary:
-	var teams = {}
+## Removes all existing teams from node
+func reset_teams_of_node(node : Node2D):
 	node.get_groups().all(
-		func(group_name : StringName):
-			if group_name in Teams: # if node has a team
-				teams[group_name] = Teams[group_name]
+		func(group_name):
+			remove_team_of_node(node, group_name)
+	)
+
+## Returns all teams of node
+func get_teams_of_node(node : Node2D) -> Dictionary:
+	var teams : Dictionary = {}
+	node.get_groups().all(
+		func(group):
+			if group in Teams: # if node has a team
+				teams[group] = Teams[group]
 	)
 	return teams
 
+## Returns first team of node
+func get_first_team_of_node(node : Node2D) -> Dictionary:
+	var teams : Dictionary = get_teams_of_node(node)
+	
+	if teams and not teams.keys().is_empty():
+		var front = teams.keys().front()
+		var team : Dictionary = {front: Teams[front]}
+		#print("Team: " + str(team))
+		return team
+	return {}
+
 ## Returns either dict or null containing the factions hostile teams
-func get_hostile_to_team(team_name : StringName) -> Dictionary:
-	var hostiles = Teams[team_name]["Hostile"]
+func get_hostile_to_team(team : Dictionary) -> Dictionary:
+	var hostiles = team[team.keys().front()]["Hostile"]
+	#print("Hostiles: " + str(hostiles))
 	if hostiles:
 		return hostiles
 	else:
 		return {}
 
 ## Returns either dict or null containing the factions friendly teams
-func get_friendly_to_team(team_name : StringName) -> Dictionary:
-	var friendlies = Teams[team_name]["Friendly"]
+func get_friendly_to_team(team : Dictionary) -> Dictionary:
+	var friendlies = team[team.keys().front()]["Friendly"]
+	#print("Friendlies: " + str(friendlies))
 	if friendlies:
 		return friendlies
 	else:
 		return {}
 
 ## Is user hostile to node
-func is_user_hostile_to_node(user, node):
-	var user_team = GameState.get_first_team_of_node(user)
-	var hostiles = GameState.get_hostile_to_team(user_team)
-	var node_team = GameState.get_first_team_of_node(node)
-	if node_team in hostiles:
-		return true
+func is_user_hostile_to_node(user : Node2D, node : Node2D):
+	if !(user == node):
+		var user_team : Dictionary = GameState.get_first_team_of_node(user)
+		var hostiles : Dictionary = GameState.get_hostile_to_team(user_team)
+		var node_teams : Dictionary = GameState.get_first_team_of_node(node)
+		for node_team in node_teams:
+			if node_team in hostiles:
+				#print("is hostile")
+				return true
+		#print("is not hostile")
 	return false
 
 ## Is user friendly to node
-func is_user_friendly_to_node(user, node):
+func is_user_friendly_to_node(user : Node2D, node : Node2D):
 	var user_team = GameState.get_first_team_of_node(user)
 	var friendlies = GameState.get_friendly_to_team(user_team)
 	var node_team = GameState.get_first_team_of_node(node)
