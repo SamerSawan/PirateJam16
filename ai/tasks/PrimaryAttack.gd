9265ee63982dde6e393ce141be053d0f66c981a7
Totsnuk <evanmaywald2@gmail.com>
2025-01-27 13:19:30 -0600

    fix task not emiting correct type for trigger

--- ai/tasks/PrimaryAttack.gd
+++ ai/tasks/PrimaryAttack.gd
@@ -7,7 +7,12 @@ func _enter() -> void:
 	if agent is Creature:
 		if not agent.primary_attack.trigger_finished.is_connected(_on_trigger_finished):
 			agent.primary_attack.trigger_finished.connect(_on_trigger_finished)
-			agent.primary_attack.trigger.emit(agent.primary_attack.attack_range.get_closest_enemy(agent), &"PrimaryAttack")
+			agent.primary_attack.trigger.emit(
+				{
+					"target_node": agent.primary_attack.attack_range.get_closest_enemy(agent), 
+					"attack_type": &"PrimaryAttack"
+				}
+			)
 			print("Primary attack triggered")
 
 func _tick(delta: float) -> Status:
