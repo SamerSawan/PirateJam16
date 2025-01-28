2bcd60c34f93261d1af2c8df4ece49320718e325
Totsnuk <evanmaywald2@gmail.com>
2025-01-28 01:52:50 -0600

    three goblins major restructure, still not working but better

--- characters/enemies/three_goblins_in_a_trench.gd
+++ characters/enemies/three_goblins_in_a_trench.gd
@@ -1,10 +1,10 @@
-extends CharacterBody2D
+extends Creature
 
 @export var speed = 150
 @export var health = 200
 
-@onready var boss_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var attack_box: Area2D = $AttackBox
+@export var vision_component : VisionComponent
 
 @export var hsm: LimboHSM
 @export var phase1: LimboState
@@ -13,7 +13,6 @@ extends CharacterBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_init_state_machine()
-
 
 func _init_state_machine():
 	hsm.add_transition(phase1, phase2, &"phase2_started")
@@ -33,17 +32,20 @@ func perform_jump_attack():
 	print("attempting jump attack")
 	#Play jump animation
 	
-	var player = get_tree().get_nodes_in_group("player")[0]
+	if vision_component.visible_targets.size() <= 0:
+		return false
 	
-	var jump_target = player.global_position
+	var target = vision_component.visible_targets.front()
+	
+	var jump_target = target.global_position
 	
 	var tween = create_tween()
-	tween.tween_property(self, "global_transform:origin", jump_target, 0.5)
+	tween.tween_property(self, "global_position", jump_target, 0.5)
 	
 	await tween
 	
 	#play land animation
 	
-	var players_in_area = $AttackBox.get_overlapping_bodies
-	for p in players_in_area:
-		player.take_damage(15)
+	var target_in_area = $AttackBox.get_overlapping_bodies
+	for p in target_in_area:
+		target.take_damage(15)
